b168d0ed29e8b0c3eb63dc994edfe9b4a9901750
Valerio Pipolo <valep27@gmail.com>
2017-05-14 13:17:51 +0200

    Use async to setup db connection and express

--- app.js
+++ app.js
@@ -1,46 +1,65 @@
 const express = require('express');
+const app = express();
 const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
-const app = express();
+const mongoose = require('mongoose');
+const async = require('async');
 
 const routes = require('./app/routes');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'app/views'));
-app.set('view engine', 'hbs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+async.series([
+  (callback) => {
+    //Set up mongoose connection
+    let mongoDB = 'localhost';
+    let db = mongoose.connection;
 
-// app routes
-app.use(routes);
+    mongoose.connect(mongoDB, callback);
+    db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+  },
+  (callback) => {
+    // express setup
+    app.set('views', path.join(__dirname, 'app/views'));
+    app.set('view engine', 'hbs');
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    // uncomment after placing your favicon in /public
+    //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+    app.use(logger('dev'));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(cookieParser());
+    app.use(express.static(path.join(__dirname, 'public')));
+    callback(null, null);
+  },
+  (callback) => {
+    // setup routes
+    app.use(routes);
+
+    // catch 404 and forward to error handler
+    app.use(function (req, res, next) {
+      var err = new Error('Not Found');
+      err.status = 404;
+      next(err);
+    });
+
+    // error handler
+    app.use(function (err, req, res, next) {
+      // set locals, only providing error in development
+      res.locals.message = err.message;
+      res.locals.error = req.app.get('env') === 'development' ? err : {};
+      // render the error page
+      res.status(err.status || 500);
+      res.render('error');
+    });
+
+    callback(null, null);
+  }
+], (error, result) => {
+  console.log('Server up and running');
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
 
 module.exports = app;
